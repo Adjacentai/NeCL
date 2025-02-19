c34853c8a4043887313b17e096613098ca179edf
Adjacentai <141495223+Adjacentai@users.noreply.github.com>
2025-02-19 22:50:05 +0700

    Update url_util.py
    delete comments

--- url_util.py
+++ url_util.py
@@ -2,22 +2,17 @@ import base64
 from urllib.parse import urlparse
 import re
 
-# Регулярное выражение для поиска стандартных ссылок на Telegram
-telegram_pattern = re.compile(r"https://t\.me/[\w]+")  # Поиск ссылок типа https://t.me/username
+telegram_pattern = re.compile(r"https://t\.me/[\w]+")  
 
-# Регулярное выражение для поиска ссылок 2ГИС
 gis_pattern = re.compile(r"https://link\.2gis\.ru/\S+")
 
 def decode_2gis_link(link):
-    # Получаем часть после последнего слэша
     encoded_part = link.split('/')[-1]
     
-    # Декодируем из Base64
     try:
         decoded_bytes = base64.urlsafe_b64decode(encoded_part)
         decoded_text = decoded_bytes.decode('utf-8')
         
-        # Ищем Telegram ссылку в декодированном тексте
         matches = re.findall(r'https://t\.me/[^\s\n]+', decoded_text)
         if matches:
             return matches[0]
@@ -28,18 +23,14 @@ def decode_2gis_link(link):
     return None
 
 def check_telegram_link(decoded_link):
-    # Проверяем наличие подстрок для Telegram
     return 't.me' in decoded_link or 'telegram.me' in decoded_link
 
 def find_telegram_links(html):
-    # Сначала ищем явные ссылки на Telegram (https://t.me/)
     direct_links = telegram_pattern.findall(html)
     
-    # Если ссылки найдены, возвращаем их
     if direct_links:
         return direct_links
 
-    # Если явных ссылок нет, ищем через 2ГИС
     links = gis_pattern.findall(html)
     telegram_links = []
 
